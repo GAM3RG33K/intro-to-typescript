8906dd147644d4c2bbc6c43eddae3f190ab3f632
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-24 13:36:05 +0530

    Add Types comment on top

--- lessons/lesson-1.js
+++ lessons/lesson-1.js
@@ -1,4 +1,21 @@
 // Types in Javascript
+// 
+// Primitives:
+// Boolean : typeof instance === "boolean"
+// Number : typeof instance === "number"
+// String : typeof instance === "string"
+// BigInt : typeof instance === "bigint"
+// Symbol : typeof instance === "symbol"
+// undefined : typeof instance === "undefined"
+
+
+// Structural Types:
+// Object : typeof instance === "object"
+// Function : typeof instance === "function"
+
+// Structural Root Primitive:
+// null : typeof instance === "object"
+
 
 function addNumbers(a, b) {
     return a + b;
