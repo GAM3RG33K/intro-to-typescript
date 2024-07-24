0ae2df3b61f06651f35b9f94201d1558988f98dc
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-24 13:35:22 +0530

    Lession 1 - Types in JS

---
+++ lessons/lesson-1.js
@@ -0,0 +1,12 @@
+// Types in Javascript
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+console.log("\nOperation: addNumbers(10, 20) \nResult: ", addNumbers(10, 20)); // Adds as numbers
+
+console.log("\naddNumbers(Operation: \"10\", \"20\") \nResult: ", addNumbers("10", "20")); // Concats as Strings
+
+console.log("\nOperation: addNumbers(\"10\", 20) \nResult: ", addNumbers("10", 20)); // Concats as Strings
+
