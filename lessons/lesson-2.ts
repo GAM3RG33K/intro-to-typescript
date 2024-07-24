5fce54d5e8384091436c03b936f0d6a41f899282
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-24 13:55:02 +0530

    Explain how types works with examples

--- lessons/lesson-2.ts
+++ lessons/lesson-2.ts
@@ -22,3 +22,38 @@
 // Never : never, Type that never returns
 // Union : a | b, Type that can be either a or b
 // Custom : type name, Type that is defined by the user
+
+
+// Implicit types
+let age = 100 // number
+let firstName = 'Angela' // string
+let busy = true // boolean
+
+
+// Explicit types
+let ageTS: number = 100
+let firstNameTS: string = 'Angela'
+let busyTS: boolean = true
+
+
+
+age = 200 // Allows re-assigning same type
+firstName = "100" // Allows re-assigning same type
+busy = false // Allows re-assigning same type
+
+ageTS = 200 // Allows re-assigning same type
+firstNameTS = "100" // Allows re-assigning same type
+busyTS = false // Allows re-assigning same type
+
+
+
+// Errors
+
+age = "200" // Error, type mismatch
+firstName = 100 // Error, type mismatch
+busy = 3.14 // Error, type mismatch
+
+ageTS = "200" // Error, type mismatch
+firstNameTS = 100 // Error, type mismatch
+busyTS = 3.14 // Error, type mismatch
+
