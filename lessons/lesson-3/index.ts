15eebc8d1e35054ee1c0747e63499f03358eb6d7
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-24 14:09:25 +0530

    Add code fixes for typescript

--- lessons/lesson-3/index.ts
+++ lessons/lesson-3/index.ts
@@ -1,15 +1,35 @@
-// Benefits of TypeScript
-// challenges:
-// - give the screens innerHTML a string
-// - change the two input values to numbers
+// // Benefits of TypeScript
+// // challenges:
+// // - give the screens innerHTML a string
+// // - change the two input values to numbers
 
+
+// // Original code 
+
+// const button = document.querySelector('.button')
+// const firstInput = document.querySelector('#first-input')
+// const secondInput = document.querySelector('#second-input')
+// const screen = document.querySelector('.screen')
+
+// function addNumbers(a,b) {
+//      screen.innerHTML = a + b     
+// }
+
+// button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
+
+// // Fixed code 
 const button = document.querySelector('.button')
-const firstInput = document.querySelector('#first-input')
-const secondInput = document.querySelector('#second-input')
-const screen = document.querySelector('.screen')
- 
-function addNumbers(a,b) {
-     screen.innerHTML = a + b     
+const firstInput = (<HTMLInputElement>document.querySelector('#first-input'))
+const secondInput = (<HTMLInputElement>document.querySelector('#second-input'))
+export const screen = document.querySelector('.screen')
+
+function addNumbers(a: number, b: number) {
+    if (screen) {
+        screen!.innerHTML = (a + b).toString()
+    }
 }
- 
-button.addEventListener('click', () => addNumbers(firstInput.value, secondInput.value))
+
+button?.addEventListener('click', () => {
+    if (firstInput && secondInput) addNumbers(parseInt(firstInput!.value), parseInt(secondInput!.value))
+})
