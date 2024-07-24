16831882cb05d1db7a7efb77dadfc6f297b01949
Harshvardhan Joshi <hj2931996@gmail.co>
2024-07-24 13:48:54 +0530

    Add Lesson 2 - Types in typescript

---
+++ lessons/lesson-2.ts
@@ -0,0 +1,24 @@
+// Types in Typescript
+// 
+// Primitives:
+// Boolean : true, false
+// Number : 10000,-200,3,4,5,1.0,-2,-0.25,-0.00000004
+// String : "hello", "world"
+// 
+
+
+// Structural Types:
+// Object : { "name": "John", "age": 30}
+// Array : [1,2,3], [true, false], ["hello", "world"]
+// Function : function() {}, function(x, y), const fun = (x,y) => { x + y }
+
+// Structural Root Primitive:
+// null : typeof instance === "object"
+// Tuple : (1,2,3),
+// Enum : { a: 1, b: 2 }
+// Any : any, Universal Type, any can be assigned to any other type
+// Unknown : unknown, Type that is not known
+// Literal : literal, Type that is explicitly defined
+// Never : never, Type that never returns
+// Union : a | b, Type that can be either a or b
+// Custom : type name, Type that is defined by the user
