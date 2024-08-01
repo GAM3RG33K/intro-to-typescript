11b044f479c17b791f6c410b0ccef3e0104954cf
Harshvardhan Joshi <hj2931996@gmail.co>
2024-08-01 12:38:09 +0530

    Lesson 7 - Tuples

---
+++ lessons/lesson-7/index.pack.js
@@ -0,0 +1,121 @@
+define("utils", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.showReviewTotal = showReviewTotal;
+    exports.populateUser = populateUser;
+    var reviewTotalDisplay = document.querySelector('#reviews');
+    var returningUserDisplay = document.querySelector('#returning-user');
+    var userNameDisplay = document.querySelector('#user');
+    function showReviewTotal(value, reviewer, isLoyalty) {
+        var iconDisplay = isLoyalty ? '⭐' : '';
+        reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
+    }
+    function populateUser(isReturning, userName) {
+        if (isReturning == true) {
+            returningUserDisplay.innerHTML = 'back';
+        }
+        userNameDisplay.innerHTML = userName;
+    }
+});
+define("index", ["require", "exports", "utils"], function (require, exports, utils_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function main_7() {
+        // Note: command to compile ts to js & open the html file in browser
+        // tsc index.ts --outFile index.pack.js
+        // open index.html
+        // Combined command: tsc index.ts utils.ts --module amd --outFile index.pack.js && open index.html
+        // Tuple Types
+        // 1. Replace the contact values to take an array that contains a
+        // phone number and email.
+        // 2. Check the inferred Type that appears in the Object Type.
+        // 3. Overwrite the inferred type to be a Tuple.
+        var propertyContainer = document.querySelector('.properties');
+        var isOpen;
+        // Reviews
+        var reviews = [
+            {
+                name: 'Sheia',
+                stars: 5,
+                loyaltyUser: true,
+                date: '01-04-2021'
+            },
+            {
+                name: 'Andrzej',
+                stars: 3,
+                loyaltyUser: false,
+                date: '28-03-2021'
+            },
+            {
+                name: 'Omar',
+                stars: 4,
+                loyaltyUser: true,
+                date: '27-03-2021'
+            },
+        ];
+        // User
+        var you = {
+            firstName: 'Bobby',
+            lastName: 'Brown',
+            isReturning: true,
+            age: 35,
+            stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+        };
+        // Array of Properties
+        var properties = [
+            {
+                image: 'images/colombia-property.jpg',
+                title: 'Colombian Shack',
+                price: 45,
+                location: {
+                    firstLine: 'shack 37',
+                    city: 'Bogota',
+                    code: 45632,
+                    country: 'Colombia'
+                },
+                contact: [+1123495082908, 'marywinkle@gmail.com'],
+                isAvailable: true
+            },
+            {
+                image: 'images/poland-property.jpg',
+                title: 'Polish Cottage',
+                price: 34,
+                location: {
+                    firstLine: 'no 23',
+                    city: 'Gdansk',
+                    code: 343903,
+                    country: 'Poland'
+                },
+                contact: [+1123495082908, 'garydavis@hotmail.com'],
+                isAvailable: false
+            },
+            {
+                image: 'images/london-property.jpg',
+                title: 'London Flat',
+                price: 23,
+                location: {
+                    firstLine: 'flat 15',
+                    city: 'London',
+                    code: 35433,
+                    country: 'United Kingdom',
+                },
+                contact: [+1123495082908, 'andyluger@aol.com'],
+                isAvailable: true
+            }
+        ];
+        // Functions
+        (0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+        (0, utils_1.populateUser)(you.isReturning, you.firstName);
+        // Add the properties
+        for (var i = 0; i < properties.length; i++) {
+            var card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = properties[i].title;
+            var image = document.createElement('img');
+            image.setAttribute('src', properties[i].image);
+            card.appendChild(image);
+            propertyContainer.appendChild(card);
+        }
+    }
+    main_7();
+});
